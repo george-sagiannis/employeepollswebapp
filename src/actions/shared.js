31422a51bea0f1749d2322f3fcb9102404a66f6f
george-sagiannis <gsagiannis@gmail.com>
2023-10-01 15:36:25 +0300

    First actions and packages modified

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { _getInitialData } from "../utils/_DATA";
+import { receiveQuestions } from "./questions";
+import { receiveUsers } from "./users";
+import { setAuthedUser } from "./authedUser";
+import { showLoading, hideLoading } from "react-redux-loading-bar";
+
+export function handleInitialData() {
+  return (dispatch) => {
+    dispatch(showLoading());
+    return _getInitialData().then(({ users, questions }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveQuestions(questions));
+      dispatch(hideLoading());
+    });
+  };
+}
