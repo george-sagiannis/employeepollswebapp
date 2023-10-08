5462555a64f5f0f4c226334b32a3043fe21aced0
george-sagiannis <gsagiannis@gmail.com>
2023-10-09 02:39:28 +0300

    README.md updated and now the is logging out

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,15 +1,10 @@
 import { SET_AUTHED_USER, LOGOUT_AUTHED_USER } from "../actions/authedUser";
 
-// Load the authedUser state from localStorage (if it exists)
-const authedUserFromStorage = localStorage.getItem("authedUser");
-
-export default function authedUser(state = authedUserFromStorage, action) {
+export default function authedUser(state = null, action) {
   switch (action.type) {
     case SET_AUTHED_USER:
-      localStorage.setItem("authedUser", action.id);
       return action.id;
     case LOGOUT_AUTHED_USER:
-      localStorage.removeItem("authedUser");
       return null;
     default:
       return state;
