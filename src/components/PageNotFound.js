ccd944e72a9880455fb18f6604d154b25755e11c
george-sagiannis <gsagiannis@gmail.com>
2023-10-09 02:42:58 +0300

    When a page does not exist, there is a button that, when clicked, redirects us to the login page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,5 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 const PageNotFound = () => {
-  return <div className="center">PageNotFound</div>;
+  const navigate = useNavigate();
+
+  const goToLogin = () => {
+    // Redirect the user to the login page
+    navigate("/login");
+  };
+
+  return (
+    <div className="center">
+      <p>Page not found</p>
+      <button onClick={goToLogin}>Go to Login</button>
+    </div>
+  );
 };
 
 export default PageNotFound;
